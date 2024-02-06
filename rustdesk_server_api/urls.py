fa3011e61e146f56290a2db640b73b2023564d08
gitondezhuts <156513740+gitondezhuts@users.noreply.github.com>
2024-02-06 23:03:53 +0000

    Update urls.py

--- rustdesk_server_api/urls.py
+++ rustdesk_server_api/urls.py
@@ -30,8 +30,6 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^api/', include('api.urls')),
     url(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT}, name='static'),
-    url(r'^canvaskit@0.33.0/(?P<path>.*)$', static.serve, {'document_root': 'static/web_client/canvaskit@0.33.0'},name='web_client'),
-
 ]
 
 from django.conf.urls import static as sc
