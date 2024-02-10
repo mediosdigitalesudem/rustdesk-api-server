bffc1d061f7698179056d864eb84561facb06678
gitondezhuts <156513740+gitondezhuts@users.noreply.github.com>
2024-02-10 22:35:01 +0000

    Update settings.py

--- rustdesk_server_api/settings.py
+++ rustdesk_server_api/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-from .secret_config import SECRET_KEY
+with suppress(ImportError):
+    from .secret_config import *  # noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
