3b80243902f36db8fa859b9eaa981e1d1c8db04a
gitondezhuts <156513740+gitondezhuts@users.noreply.github.com>
2024-02-19 00:26:15 +0000

    Update settings.py

--- rustdesk_server_api/settings.py
+++ rustdesk_server_api/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from contextlib import suppress
 import os
 from pathlib import Path
-from .secret_config import CSRF_TRUSTED_ORIGINS
+from .secret_config import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS = [CSRF_TRUSTED_ORIGINS]
+CSRF_TRUSTED_ORIGINS = [CSRF_TRUSTED_ORIGINS] if isinstance(CSRF_TRUSTED_ORIGINS, str) else CSRF_TRUSTED_ORIGINS
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
